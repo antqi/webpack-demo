bba6de22dcb528300519a4c96cb22b4f3192cbd9
antqi <hi.antqi@gmail.com>
2017-11-07 15:23:24 +0800

    add css

--- app/index.js
+++ app/index.js
@@ -1,3 +1,5 @@
 import component from './component'
 
+import './common/css.css'
+
 document.body.appendChild(component('Hello World V1.1.0'))
--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,38 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
+  build: path.join(__dirname, 'build'),
 }
 
 module.exports = {
   devServer: {
     host: process.env.HOST,
     // port: process.env.PORT
-    port: 9222
+    port: 9222,
   },
   entry: {
-    app: PATHS.app
+    app: PATHS.app,
   },
   output: {
     path: PATHS.build,
-    filename: '[name].js'
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Webpack demo'
-    })
-  ]
+      title: 'Webpack demo',
+    }),
+  ],
 }
